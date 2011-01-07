26f52aae5620e01a15a69dd68c4b8eb113443265
oupo <oupo.nejiki@gmail.com>
2011-01-08 00:39:00 +0900

    inzm3-practicegame-dump-ogre.rbに修練場ボスのダンプを追加

--- inzm3-practicegame-dump-ogre.rb
+++ inzm3-practicegame-dump-ogre.rb
@@ -45,8 +45,57 @@ GameData = Struct.new(:name, :game_id)
 all_result = []
 
 def add_result(result, line, name=nil)
-	game_name, drop_items, drop_odds, exp, nekketu, yuujou = eval(line)
-	result << "\t{name: %p, drop_items: %p, drop_odds: %p, exp: %d, nekketu: %d, yuujou: %d}" % [name || game_name, drop_items, drop_odds, exp, nekketu, yuujou]
+	game_name, drop_items, drop_odds, exp, nekketu, yuujou = *line
+	result << [name || game_name, drop_items, drop_odds, exp, nekketu, yuujou]
+end
+
+def add_dungeon_boss_result(all_result, games_dump)
+	dgncrs = read_slice("dgncrs.dat", 20)
+	dgnmatch = read_slice("dgnmatch.dat", 256)
+	course_no_to_name = %w(アタック ディフェンス スピード テクニック たいりょく ふしぎ 経験値 スーパー経験値)
+	chapter_range = 5..11
+	num_base_courses = 6
+	
+	result = []
+	all_result << ["地下修練場 ボス", result]
+	
+	chapter_range.each do |chapter|
+		chapter_name = chapter == 11 ? "クリア後" : "#{chapter}章"
+		
+		game_ids = []
+		course_no_to_name.size.times do |course|
+			pos = (chapter - 1) * 9 + course
+			crs = dgncrs[pos]
+			match_index = read16(crs, 0x10) - 1
+			match = dgnmatch[match_index]
+			boss_game_id = read16(match, 4)
+			game_ids << boss_game_id
+		end
+		
+		bundle_game_id_and_course(game_ids).each do |(game_id, courses)|
+			if courses.length == num_base_courses
+				course_names_str = ""
+			else
+				course_names_str = " " + courses.map {|course| course_no_to_name[course] }.join("・")
+			end
+			add_result result, games_dump[game_id], "修練場 #{chapter_name}#{course_names_str}"
+		end
+		
+	end
+end
+
+def bundle_game_id_and_course(game_ids)
+	result = []
+	game_id_to_result_index = {}
+	game_ids.each_with_index do |game_id, course|
+		if game_id_to_result_index.has_key?(game_id)
+			result[game_id_to_result_index[game_id]][1] << course
+		else
+			game_id_to_result_index[game_id] = result.length
+			result << [game_id, [course]]
+		end
+	end
+	result
 end
 
 Dir.glob("PracticeGame*F.dat") do |path|
@@ -65,18 +114,29 @@ Dir.glob("PracticeGame*F.dat") do |path|
 		if route_offset == 0
 			result = []
 			all_result << ["#{practice_name} - #{route_name}", result]
-			puts "#{practice_name} - #{route_name}"
 		end
 		
 		name, game_id = game_data.name, game_data.game_id
 		
-		game_name, drop_items, drop_odds, exp, nekketu, yuujou = games_dump[game_id]
-		drop_text = [drop_items, drop_odds].transpose.map{|name,odd| "#{name.chomp("　")}(#{odd}%)"}.join(",")
-		puts " #{name}:#{drop_text},#{exp},#{nekketu},#{yuujou}"
-		#add_result result, games_dump[game_id]
+		add_result result, games_dump[game_id], name
 	end
 end
 
-all_result.map {|(category_name, result)|
-	"%p: [\n%s\n]" % [category_name, result.join(",\n")]
-}.join(",\n")
+add_dungeon_boss_result all_result, games_dump
+
+
+if true # text dump
+	all_result.each do |(category_name, result)|
+		puts "#{category_name}"
+		result.each do |(name, drop_items, drop_odds, exp, nekketu, yuujou)|
+			drop_text = [drop_items, drop_odds].transpose.map{|name,odd| "#{name.chomp("　")}(#{odd}%)"}.join(",")
+			puts " #{name}:#{drop_text},#{exp},#{nekketu},#{yuujou}"
+		end
+	end
+else # js dump
+	puts all_result.map {|(category_name, result)|
+		"%p: [\n%s\n]" % [category_name, result.map{|(name, drop_items, drop_odds, exp, nekketu, yuujou)|
+			"\t{name: %p, drop_items: %p, drop_odds: %p, exp: %d, nekketu: %d, yuujou: %d}" % [name, drop_items, drop_odds, exp, nekketu, yuujou]
+		}.join(",\n")]
+	}.join(",\n")
+end
