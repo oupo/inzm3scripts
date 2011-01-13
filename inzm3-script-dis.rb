3c4344015db1d17251b239416dd58a4ba35eff3b
oupo <oupo.nejiki@gmail.com>
2011-01-13 16:46:18 +0900

    inzm3-script-dis.rb: TYPE_NAMES追加。ほかからexprで参照されているblockをインデント

--- inzm3-script-dis.rb
+++ inzm3-script-dis.rb
@@ -30,13 +30,62 @@ def read_string_pool(buf)
 end
 
 TYPE_NAMES = {
+	0x406a => "get_battle_unit",
+	0x40b7 => "exist_eve_script",
+	0x40bd => "unit_gender",
 	0x7001 => "write",
+	0x7003 => "eq",
+	0x7009 => "add",
+	0x700a => "sub",
 	0x7010 => "rand",
 	0x3070 => "debug_print",
+	0x6001 => "begin",
+	0x6002 => "end",
+	0x6008 => "if_eq0",
+	0x600c => "else",
 }
 
 def disasm_script(buf)
 	string_pool = read_string_pool(buf)
+	nestlevels = get_nestlevels_of_block(buf)
+	max_nestlevel = nestlevels.max_by{|x| x || 0 }
+	
+	each_blocks(buf) do |i, pc, label, size, type|
+		name = TYPE_NAMES[type]
+		args = dump_args(buf, pc, string_pool)
+		nestlevel = nestlevels[label]
+		
+		puts "%sblock%s#%04x: size=%d, type=%.4x%s, args=[%s]" %
+		      [" " * nestlevel, " " * (max_nestlevel - nestlevel), label, size, type, name ? " (#{name})" : "", args.join(", ")]
+	end
+end
+
+def get_nestlevels_of_block(buf)
+	result = []
+	label_to_pc = []
+	
+	do_block = lambda {|pc, level|
+		label = read16(buf, pc + 0)
+		if not result[label]
+			result[label] = level
+			each_args(buf, pc) do |i, type, x|
+				if type == 4
+					do_block.(label_to_pc[x], level + 1)
+				end
+			end
+		end
+	}
+	
+	each_blocks(buf) do |i, pc, label, size, type|
+		label_to_pc[label] = pc
+	end
+	each_blocks(buf) do |i, pc, label, size, type|
+		do_block.(pc, 0)
+	end
+	result
+end
+
+def each_blocks(buf)
 	num_blocks = read16(buf, 0xc)
 	pc = 0x20
 	
@@ -44,13 +93,18 @@ def disasm_script(buf)
 		label = read16(buf, pc + 0)
 		size = read16(buf, pc + 2)
 		type = read16(buf, pc + 4)
-		name = TYPE_NAMES[type]
-		
-		args = dump_args(buf, pc, string_pool)
-		
-		puts "block#%d: label=%.4x, size=%d, type=%.4x%s, args=[%s]" %
-		      [i, label, size, type, name ? " (#{name})" : "", args.join(", ")]
+		yield i, pc, label, size, type
 		pc += size
+	end
+end
+
+def each_args(buf, pc)
+	nargs = read8(buf, pc + 6)
+	pos = pc + 8 + ((nargs + 7) / 8) * 4
+	nargs.times do |i|
+		type = (read32(buf, pc + 8 + (i / 8 * 4)) >> (i % 8 * 4)) & 0xf
+		x = read32(buf, pos + i * 4)
+		yield i, type, x
 	end
 end
 
@@ -60,12 +114,8 @@ end
 
 def dump_args(buf, pc, string_pool)
 	label = read16(buf, pc + 0)
-	nargs = read8(buf, pc + 6)
-	pos = pc + 8 + ((nargs + 7) / 8) * 4
 	r = []
-	nargs.times do |i|
-		type = (read32(buf, pc + 8 + (i / 8 * 4)) >> (i % 8 * 4)) & 0xf
-		x = read32(buf, pos + i * 4)
+	each_args(buf, pc) do |i, type, x|
 		case type
 		when 1, 2, 3
 			str = get_string_from_pool(string_pool, label, i)
